5084fd706c6e517849b89ebcbe99ef698a04b2a1
prudhvi krishna Narra <prudhvikrishna.narra1@marist.edu>
2014-02-21 01:08:24 -0500

    Lab5

---
+++ Lab5.sql
@@ -0,0 +1,48 @@
+﻿--1. Get the cities of agents booking an order for customer “Basics”. This time use joins; no subqueries.
+
+select a.city from agents a
+join orders o
+on o.aid=a.aid
+join customers c
+on c.cid=o.cid
+where c.name='Basics'
+
+--2. Get the pids of products ordered through any agent who makes at least one order for a customer in Kyoto. Use joins this time; no subqueries.
+
+select distinct o1.pid from orders o
+left join customers c
+on o.cid=c.cid
+join orders o1
+on o.aid=o1.aid
+where c.city='Kyoto' order by pid asc
+
+--3. Get the names of customers who have never placed an order. Use a subquery.
+
+select distinct name from Customers
+where not cid in (
+      select cid from Orders
+      )
+
+--4. Get the names of customers who have never placed an order. Use an outer join.
+
+select name from customers c
+left outer join orders o
+           on o.cid=c.cid
+           where o.cid is null
+
+--5. Get the names of customers who placed at least one order through an agent in their city, along with those agent(s’) names.
+
+select distinct c.name as cname, a.name as aname
+from Customers c, Agents a, Orders o
+where c.city = a.city 
+and o.aid = a.aid
+and o.cid = c.cid
+
+--6. Get the names of customers and agents in the same city, along with the name of the city, regardless
+--of whether or not the customer has ever placed an order with that agent.
+
+select c.name as cname,a.name as aname
+from customers c,agents a
+where c.city=a.city
+
+--7. Get the name and city of customers who live in the city where the least number of products are made.
